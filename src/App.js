27d066f9a69627f903fddc73ae06dfeb508efa59
datdev266 <datdev266@gmail.com>
2022-10-26 14:36:17 +0700

    replace code app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import "./App.css";
 import Weather from "./components/Weather";
 import { Calo } from "./quy/components/Calo";
@@ -10,39 +10,36 @@ import { Signin } from "./quy/components/Signin";
 import { Signup } from "./quy/components/Signup";
 import { Tdee } from "./quy/components/Tdee";
 import { TdeeContext } from "./quy/context/Context";
-function App() {
-  const tdee = useContext(TdeeContext);
-import { Calo } from "./quy/components/Calo";
+
 import { useNavigate } from "react-router";
-import { useEffect } from "react";
+
 function App() {
-  const tdee = useContext(TdeeContext);
-  const [city, setCity] = useState("");
+    const tdee = useContext(TdeeContext);
+    const [city, setCity] = useState("");
 
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/");
+    }, [tdee.logged]);
 
-  useEffect(() => {
-    navigate("/");
-  }, [tdee.logged]);
+    return (
+        <div className="App">
+            {/* <Header /> */}
+            {tdee.logged && <Navbar />}
+            <Signin />
+            <Signup />
 
-  return (
-    <div className="App">
-      {/* <Header /> */}
-      {tdee.logged && <Navbar />}
-      <Signin />
-      <Signup />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tdee" element={<Tdee />} />
-        <Route path="/calo" element={<Calo />} />
-        <Route path="/weather" element={<Weather />} />
-        {/* <Route path="/auth/login" element={<Login />} />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/tdee" element={<Tdee />} />
+                <Route path="/calo" element={<Calo />} />
+                <Route path="/weather" element={<Weather />} />
+                {/* <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} /> */}
-      </Routes>
-    </div>
-  );
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
